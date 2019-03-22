6236eb525517b5988e5dc9dc6cb75b478d53ca12
Daichi Yoshida <yoshida@jsk.co.jp>
2019-03-22 17:19:12 +0900

    ヘッダー追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <v-app>
+      <nav-menu></nav-menu>
       <v-container>
-        <v-layout>
-          <v-flex xs4>
+        <v-layout wrap>
+          <v-flex xs12 sm3 md3>
             <v-btn color="success">Success</v-btn>
           </v-flex>
-          <v-flex xs4>
+          <v-flex xs12 sm3 md3>
             <v-btn color="error">Error</v-btn>
           </v-flex>
-          <v-flex xs4>
+          <v-flex xs12 sm3 md3>
             <v-btn color="warning">Warning</v-btn>
           </v-flex>
-          <v-flex xs4>
+          <v-flex xs12 sm3 md3>
             <v-btn color="info">Info</v-btn>
           </v-flex>
         </v-layout>
@@ -23,7 +24,12 @@
 </template>
 
 <script>
+import NavMenu from "../components/NavMenu";
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    NavMenu
+  }
 };
 </script>
