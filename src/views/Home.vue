b5cf5c81cb536df6ae24f254b5a6420313f89b49
Daichi Yoshida <yoshida@jsk.co.jp>
2019-03-22 16:34:32 +0900

    vuetify適応

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/logo.png">
+    <v-app>
+      <v-container>
+        <v-layout>
+          <v-flex xs4>
+            <v-btn color="success">Success</v-btn>
+          </v-flex>
+          <v-flex xs4>
+            <v-btn color="error">Error</v-btn>
+          </v-flex>
+          <v-flex xs4>
+            <v-btn color="warning">Warning</v-btn>
+          </v-flex>
+          <v-flex xs4>
+            <v-btn color="info">Info</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-app>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
