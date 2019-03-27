cd7378fa1e1831aaf8473723a71001b44811be80
Daichi Yoshida <d.y.0110.barcelona@gmail.com>
2019-03-27 23:33:40 +0900

    サイドメニューの実装

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,18 +1,47 @@
 <template>
-  <v-toolbar color="info">
-    <v-toolbar-side-icon></v-toolbar-side-icon>
-    <v-toolbar-title>Title</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <!-- <v-btn flat>Link One</v-btn>
-      <v-btn flat>Link Two</v-btn>
-      <v-btn flat>Link Three</v-btn> -->
-    </v-toolbar-items>
-  </v-toolbar>
+  <div id="navmenu">
+    <v-toolbar color="info">
+      <v-toolbar-side-icon class="white--text" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list class="pt-0" dense>
+        <v-list-tile></v-list-tile>
+        <v-list-tile>
+          <!-- <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>-->
+
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <router-link class="title" to="/">Home</router-link>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <!-- <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>-->
+
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <router-link class="title" to="/about">About</router-link>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
 export default {
- name: "NavMenu"    
-}
+  name: "NavMenu",
+  data() {
+    return {
+      drawer: null
+    };
+  }
+};
 </script>
