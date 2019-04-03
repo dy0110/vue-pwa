cb1a1332a493b3cfce829f098988ffcffedcb653
Daichi Yoshida <d.y.0110.barcelona@gmail.com>
2019-04-03 23:23:24 +0900

    ルーティング検査

--- src/views/ItemDetail.vue
+++ src/views/ItemDetail.vue
@@ -2,13 +2,19 @@
   <!-- <div>{{ $route.params.itemId }}</div> -->
   <v-card>
     <v-card-title>
-      <span class="title font-weight-light">テスト</span>
+      <span class="title font-weight-light">{{itemDetail[0].title}}</span>
     </v-card-title>
     <v-card-text>
-      <span>ほげ</span>
+      <v-flex xs12 sm3>
+        <a :href="itemDetail[0].public_url">
+          <v-btn flat icon color="indigo">
+            <v-icon>web</v-icon>
+          </v-btn>
+        </a>
+      </v-flex>
     </v-card-text>
     <v-card-actions>
-      <v-btn flat dark>お気に入り</v-btn>
+      <v-btn flat>お気に入り</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -26,6 +32,7 @@ export default {
   created() {
     const itemId = Number(this.$route.params.itemId);
     this.itemDetail = store.getters.getDetailData(itemId);
+    console.log(this.itemDetail);
   }
 };
 </script>
