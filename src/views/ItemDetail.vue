15600b1382e4c05afdadf96b684addea54c284ef
Daichi Yoshida <yoshida@jsk.co.jp>
2019-04-13 15:31:45 +0900

    カードデザイン変更

--- src/views/ItemDetail.vue
+++ src/views/ItemDetail.vue
@@ -2,13 +2,41 @@
   <!-- <div>{{ $route.params.itemId }}</div> -->
   <v-card>
     <v-card-title>
-      <span class="title font-weight-light">テスト</span>
+      <span class="title font-weight-light">{{ itemDetail[0].title }}</span>
     </v-card-title>
     <v-card-text>
-      <span>ほげ</span>
+      <div v-if="itemDetail[0].city_name === ''">
+        <span>聖地: なし</span>
+      </div>
+      <div v-else>
+        <span>聖地: {{itemDetail[0].city_name}}</span>
+      </div>
     </v-card-text>
     <v-card-actions>
-      <v-btn flat dark>お気に入り</v-btn>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn flat fab dark color="pink" v-on="on">
+            <v-icon dark>favorite_border</v-icon>
+          </v-btn>
+        </template>
+        <span>お気に入り</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn flat fab dark color="success" v-on="on" @click="openPublicUrl">
+            <v-icon dark>web</v-icon>
+          </v-btn>
+        </template>
+        <span>公式サイトへ</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn flat fab dark color="info" v-on="on" @click="openTwitter">
+            ツイッター
+          </v-btn>
+        </template>
+        <span>ツイッターへ</span>
+      </v-tooltip>
     </v-card-actions>
   </v-card>
 </template>
@@ -22,6 +50,14 @@ export default {
     return {
       itemDetail: null
     };
+  },
+  methods: {
+    openPublicUrl() {
+      window.open(this.itemDetail[0].public_url);
+    },
+    openTwitter(){
+      window.open( "https://twitter.com/" );
+    }
   },
   created() {
     const itemId = Number(this.$route.params.itemId);
